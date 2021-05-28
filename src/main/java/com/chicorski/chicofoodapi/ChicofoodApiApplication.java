22a20b69c797cd4242c043aa7310fcb6fb5a292c
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-05-27 21:58:59 -0300

    Initial commit

---
+++ src/main/java/com/chicorski/chicofoodapi/ChicofoodApiApplication.java
@@ -0,0 +1,13 @@
+package com.chicorski.chicofoodapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChicofoodApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChicofoodApiApplication.class, args);
+	}
+
+}
