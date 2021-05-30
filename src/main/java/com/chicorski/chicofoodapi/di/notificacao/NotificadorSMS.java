95b27d6fcb04cf75591b8087dab6488eeee9c9a8
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-05-30 11:35:04 -0300

    Ambiguidade de beans e injeção de lista de beans

---
+++ src/main/java/com/chicorski/chicofoodapi/di/notificacao/NotificadorSMS.java
@@ -0,0 +1,13 @@
+package com.chicorski.chicofoodapi.di.notificacao;
+
+import com.chicorski.chicofoodapi.di.modelo.Cliente;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificadorSMS implements Notificador {
+
+    public void notificar(Cliente cliente, String mensagem) {
+        System.out.printf("Notificando %s por SMS através do telefone %s: %s\n",
+                cliente.getNome(), cliente.getTelefone(), mensagem);
+    }
+}
--- src/main/java/com/chicorski/chicofoodapi/di/service/AtivicaoClienteService.java
+++ src/main/java/com/chicorski/chicofoodapi/di/service/AtivicaoClienteService.java
@@ -5,19 +5,19 @@ import com.chicorski.chicofoodapi.di.notificacao.Notificador;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class AtivicaoClienteService {
 
-    @Autowired(required = false)
-    private Notificador notificador;
+    @Autowired
+    private List<Notificador> notificadores;
 
     public void ativar(Cliente cliente) {
         cliente.ativar();
 
-        if (notificador != null) {
+        for (Notificador notificador : notificadores) {
             notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
-        } else {
-            System.out.println("Não existe notificador, mas cliente foi ativado!");
         }
 
     }
