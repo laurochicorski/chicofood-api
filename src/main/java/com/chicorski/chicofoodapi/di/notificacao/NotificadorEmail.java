4efd0722c9839377129a350f59fa9e30aee074ab
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-05-30 18:19:36 -0300

    Criando e acessando propridades customizadas com @Value

--- src/main/java/com/chicorski/chicofoodapi/di/notificacao/NotificadorEmail.java
+++ src/main/java/com/chicorski/chicofoodapi/di/notificacao/NotificadorEmail.java
@@ -1,6 +1,7 @@
 package com.chicorski.chicofoodapi.di.notificacao;
 
 import com.chicorski.chicofoodapi.di.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
@@ -9,11 +10,19 @@ import org.springframework.stereotype.Component;
 @Component
 public class NotificadorEmail implements Notificador {
 
+    @Value("${notificador.email.host-servidor}")
+    private String host;
+
+    @Value("${notificador.email.porta-servidor}")
+    private Integer porta;
+
     public NotificadorEmail() {
         System.out.println("Notificador e-mail real.");
     }
 
     public void notificar(Cliente cliente, String mensagem) {
+        System.out.println("Host: " + host);
+        System.out.println("Port: " + porta);
         System.out.printf("Notificando %s através do e-mail %s: %s\n",
                 cliente.getNome(), cliente.getEmail(), mensagem);
     }
