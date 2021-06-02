32a17f9a372ce1dcea559883a293cd5db2552da2
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-06-02 01:16:03 -0300

    Buscando um objeto pelo id no banco de dados

---
+++ src/main/java/com/chicorski/chicofoodapi/jpa/BuscaCozinhaMain.java
@@ -0,0 +1,24 @@
+package com.chicorski.chicofoodapi.jpa;
+
+import com.chicorski.chicofoodapi.ChicofoodApiApplication;
+import com.chicorski.chicofoodapi.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class BuscaCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(ChicofoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+        
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+        
+        Cozinha cozinha = cadastroCozinha.buscar(1L);
+
+        System.out.println(cozinha.getNome());
+    }
+}
