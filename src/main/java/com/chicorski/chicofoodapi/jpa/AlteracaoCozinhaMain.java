a647af4d73713afc57d7183694150a5b8fb25683
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-06-02 09:31:49 -0300

    Atualizando um objeto no banco de dados

--- src/main/java/com/chicorski/chicofoodapi/jpa/AlteracaoCozinhaMain.java
+++ src/main/java/com/chicorski/chicofoodapi/jpa/AlteracaoCozinhaMain.java
@@ -6,7 +6,7 @@ import org.springframework.boot.WebApplicationType;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ApplicationContext;
 
-public class InclusaoCozinhaMain {
+public class AlteracaoCozinhaMain {
 
     public static void main(String[] args) {
         ApplicationContext applicationContext = new SpringApplicationBuilder(ChicofoodApiApplication.class)
@@ -15,16 +15,14 @@ public class InclusaoCozinhaMain {
         
         CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
 
-        Cozinha cozinha1 = new Cozinha();
-        cozinha1.setNome("Japanesa");
-        Cozinha cozinha2 = new Cozinha();
-        cozinha2.setNome("Italiana");
+        Cozinha cozinha = new Cozinha();
+        cozinha.setId(1L);
+        cozinha.setNome("Brasileira");
 
-        cozinha1 =cadastroCozinha.salvar(cozinha1);
-        cozinha2 = cadastroCozinha.salvar(cozinha2);
+        cozinha =cadastroCozinha.salvar(cozinha);
 
-        System.out.printf("%d = %s\n", cozinha1.getId(), cozinha1.getNome());
-        System.out.printf("%d = %s\n", cozinha2.getId(), cozinha2.getNome());
+        System.out.printf("%d = %s\n", cozinha.getId(), cozinha.getNome());
+
 
     }
 }
