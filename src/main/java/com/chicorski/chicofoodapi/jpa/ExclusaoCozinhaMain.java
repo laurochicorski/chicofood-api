9da929c1e2d57798a8823034e87583f358c7a7cd
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-06-02 09:38:27 -0300

    Excluindo um objeto no banco de dados

---
+++ src/main/java/com/chicorski/chicofoodapi/jpa/ExclusaoCozinhaMain.java
@@ -0,0 +1,23 @@
+package com.chicorski.chicofoodapi.jpa;
+
+import com.chicorski.chicofoodapi.ChicofoodApiApplication;
+import com.chicorski.chicofoodapi.domain.model.Cozinha;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+public class ExclusaoCozinhaMain {
+
+    public static void main(String[] args) {
+        ApplicationContext applicationContext = new SpringApplicationBuilder(ChicofoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+        
+        CadastroCozinha cadastroCozinha = applicationContext.getBean(CadastroCozinha.class);
+
+        Cozinha cozinha = new Cozinha();
+        cozinha.setId(1L);
+
+        cadastroCozinha.remover(cozinha);
+    }
+}
