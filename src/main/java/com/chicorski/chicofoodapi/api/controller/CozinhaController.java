904bfe2abadad69219ae21598e3838f1a48d3744
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-06-08 18:52:29 -0300

    Corrigindo o Status HTTP para resource inexistente

--- src/main/java/com/chicorski/chicofoodapi/api/controller/CozinhaController.java
+++ src/main/java/com/chicorski/chicofoodapi/api/controller/CozinhaController.java
@@ -34,14 +34,12 @@ public class CozinhaController {
     public ResponseEntity<Cozinha> buscar(@PathVariable("cozinhaId") Long id) {
         Cozinha cozinha = cozinhaRepository.buscar(id);
 
-        HttpHeaders headers = new HttpHeaders();
-        headers.add(HttpHeaders.LOCATION, "http://localhost:8180/cozinhas");
+        if (cozinha != null) {
+            return ResponseEntity.ok(cozinha);
+        }
 
-        return ResponseEntity.ok(cozinha);
-//        return ResponseEntity
-//                .status(HttpStatus.FOUND)
-//                .headers(headers)
-//                .build();
-
+        return ResponseEntity
+                .notFound()
+                .build();
     }
 }
