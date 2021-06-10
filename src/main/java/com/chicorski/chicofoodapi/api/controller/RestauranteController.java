931566725b61040c1e614c3d01bde1e8804ef13e
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-06-09 22:52:20 -0300

    Finalizando a atualização parcial com a API de Reflections do Sptring

--- src/main/java/com/chicorski/chicofoodapi/api/controller/RestauranteController.java
+++ src/main/java/com/chicorski/chicofoodapi/api/controller/RestauranteController.java
@@ -6,12 +6,15 @@ import com.chicorski.chicofoodapi.domain.model.Restaurante;
 import com.chicorski.chicofoodapi.domain.repository.CozinhaRepository;
 import com.chicorski.chicofoodapi.domain.repository.RestauranteRepository;
 import com.chicorski.chicofoodapi.domain.service.CadastroRestauranteService;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.ReflectionUtils;
 import org.springframework.web.bind.annotation.*;
 
+import java.lang.reflect.Field;
 import java.util.List;
 import java.util.Map;
 
@@ -88,12 +91,22 @@ public class RestauranteController {
             return ResponseEntity.notFound().build();
         }
 
+        merge(campos, restauranteAtual);
+
         return atualizar(id, restauranteAtual);
     }
 
     private void merge(Map<String, Object> campos, Restaurante restaurante) {
+        ObjectMapper objectMapper = new ObjectMapper();
+        Restaurante restauranteOrigem = objectMapper.convertValue(campos, Restaurante.class);
+
         campos.forEach((nome, valor) -> {
-            System.out.println(nome + " = " + valor);
+            Field field = ReflectionUtils.findField(Restaurante.class, nome);
+            field.setAccessible(true);
+
+            Object novoValor = ReflectionUtils.getField(field, restauranteOrigem);
+
+            ReflectionUtils.setField(field, restaurante, novoValor);
         });
     }
 }
