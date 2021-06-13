be31a1fa2015da94236893043dae4ca181330dff
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-06-13 11:10:19 -0300

    Externalizando consultas JPQL para um arquivo XML

--- src/main/java/com/chicorski/chicofoodapi/domain/repository/RestauranteRepository.java
+++ src/main/java/com/chicorski/chicofoodapi/domain/repository/RestauranteRepository.java
@@ -13,8 +13,9 @@ public interface RestauranteRepository extends JpaRepository<Restaurante, Long> 
 
     List<Restaurante> findByTaxaFreteBetween(BigDecimal taxaInicial, BigDecimal taxaFinal);
 
-    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
-    List<Restaurante> consultarPorNome(String nome, @Param("id") Long cozinhaId);
+//    @Query("from Restaurante where nome like %:nome% and cozinha.id = :id")
+
+    List<Restaurante> consultarPorNome(String nome, Long cozinhaId);
 
     List<Restaurante> findByNomeContainingAndCozinhaId(String nome, Long cozinhaId);
 
