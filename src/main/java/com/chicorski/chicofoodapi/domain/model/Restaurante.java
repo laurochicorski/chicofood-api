26fec6bff3ddd7e334d9a332447a1ddeb5113702
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-06-15 19:53:20 -0300

    Mapeando relacionamento muitos-para-muitos com @ManyToMany

--- src/main/java/com/chicorski/chicofoodapi/domain/model/Restaurante.java
+++ src/main/java/com/chicorski/chicofoodapi/domain/model/Restaurante.java
@@ -5,6 +5,8 @@ import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Data
@@ -26,6 +28,12 @@ public class Restaurante {
     @ManyToOne
     @JoinColumn(name = "cozinha_id", nullable = false)
     private Cozinha cozinha;
+
+    @ManyToMany
+    @JoinTable(name = "restaurante_forma_pagamento"
+            , joinColumns = @JoinColumn(name="restaurante_id")
+            , inverseJoinColumns = @JoinColumn(name="forma_pagamento_id"))
+    private List<FormaPagamento> formasPagamento = new ArrayList<>();
 
     public Long getId() {
         return id;
