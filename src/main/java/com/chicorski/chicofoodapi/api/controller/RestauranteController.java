7fb2105c8ed32ab9f062470d4be5e28b887f4a50
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-06-15 22:35:20 -0300

    Testando e analisando o impacto da incorporação de classe na REST API

--- src/main/java/com/chicorski/chicofoodapi/api/controller/RestauranteController.java
+++ src/main/java/com/chicorski/chicofoodapi/api/controller/RestauranteController.java
@@ -71,7 +71,7 @@ public class RestauranteController {
             Optional<Restaurante> restauranteAtual = restauranteRepository.findById(id);
 
             if (restauranteAtual.isPresent()) {
-                BeanUtils.copyProperties(restaurante, restauranteAtual.get(), "id");
+                BeanUtils.copyProperties(restaurante, restauranteAtual.get(), "id", "formasPagamento", "endereco");
 
                 Restaurante restauranteSalvo = cadastroRestaurante.salvar(restauranteAtual.get());
                 return ResponseEntity.ok(restauranteSalvo);
