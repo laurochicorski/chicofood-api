e61d290c013e2ce346d721719585b88bb360140e
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-06-15 23:16:47 -0300

    Desafio: mapeando relacionamentos muitos-para-muitos

---
+++ src/main/java/com/chicorski/chicofoodapi/domain/model/Grupo.java
@@ -0,0 +1,25 @@
+package com.chicorski.chicofoodapi.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Grupo {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @ManyToMany
+    @JoinTable(name = "grupo_permissao", joinColumns = @JoinColumn(name = "grupo_id"),
+                inverseJoinColumns = @JoinColumn(name = "permissao_id"))
+    private List<Permissao> permissoes;
+}
