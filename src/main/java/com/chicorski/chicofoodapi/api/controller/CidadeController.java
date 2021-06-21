0a9bfc5c91fc29f5d1787857772ba2e2b3435813
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-06-21 09:45:02 -0300

    Tratando exceções globais com @ExceptionHandler e @ControllerAdvice

--- src/main/java/com/chicorski/chicofoodapi/api/controller/CidadeController.java
+++ src/main/java/com/chicorski/chicofoodapi/api/controller/CidadeController.java
@@ -1,20 +1,15 @@
 package com.chicorski.chicofoodapi.api.controller;
 
-import com.chicorski.chicofoodapi.api.exceptionHandler.Problema;
-import com.chicorski.chicofoodapi.domain.exception.EntidadeNaoEncontradaException;
 import com.chicorski.chicofoodapi.domain.exception.EstadoNaoEncontradaException;
 import com.chicorski.chicofoodapi.domain.exception.NegocioException;
 import com.chicorski.chicofoodapi.domain.model.Cidade;
-import com.chicorski.chicofoodapi.domain.model.Estado;
 import com.chicorski.chicofoodapi.domain.repository.CidadeRepository;
 import com.chicorski.chicofoodapi.domain.service.CadastroCidadeService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 @RestController
@@ -64,27 +59,5 @@ public class CidadeController {
     @ResponseStatus(HttpStatus.NO_CONTENT)
     private void remover(@PathVariable Long id) {
         cadastroCidade.excluir(id);
-    }
-
-    @ExceptionHandler(EntidadeNaoEncontradaException.class)
-    public ResponseEntity<?> tratarEntidadeNaoEncontradaException(EntidadeNaoEncontradaException e) {
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage())
-                .build();
-
-        return ResponseEntity.status(HttpStatus.NOT_FOUND)
-                .body(problema);
-    }
-
-    @ExceptionHandler(NegocioException.class)
-    public ResponseEntity<?> tratarNegocioException(NegocioException e) {
-        Problema problema = Problema.builder()
-                .dataHora(LocalDateTime.now())
-                .mensagem(e.getMessage())
-                .build();
-
-        return ResponseEntity.status(HttpStatus.BAD_REQUEST)
-                .body(problema);
     }
 }
