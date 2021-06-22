7f240a9850b778fa57aeda99a7d3187596aa2643
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-06-22 19:00:12 -0300

    Desafio: tratando exception de violação de constraint de validação

--- src/main/java/com/chicorski/chicofoodapi/api/exceptionHandler/ProblemType.java
+++ src/main/java/com/chicorski/chicofoodapi/api/exceptionHandler/ProblemType.java
@@ -10,13 +10,14 @@ public enum ProblemType {
     ENTIDADE_EM_USO("/entidade-em-uso", "Entidade em uso" ),
     ERRO_NEGOCIO("/erro-negocio", "Violação de regra de negócio"),
     PARAMETRO_INVALIDO("/parametro-invalido", "Parametro inválido"),
-    ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema");
+    ERRO_DE_SISTEMA("/erro-de-sistema", "Erro de sistema"),
+    DADOS_INVALIDOS("/dados-invalidos", "Dados inválidos");
 
     private String title;
     private String uri;
 
     ProblemType (String path, String title) {
-        this.uri = "https://chicofood.com/" + path;
+        this.uri = "https://chicofood.com" + path;
         this.title = title;
     }
 }
