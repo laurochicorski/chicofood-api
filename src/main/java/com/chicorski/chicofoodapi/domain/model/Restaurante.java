95d84b8674a351e70e96ee1d9cd70f427ec0dad7
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-06-23 10:59:35 -0300

    Customizando e resolvendo mensagens de validação globais em Resource Bundle

--- src/main/java/com/chicorski/chicofoodapi/domain/model/Restaurante.java
+++ src/main/java/com/chicorski/chicofoodapi/domain/model/Restaurante.java
@@ -28,7 +28,7 @@ public class Restaurante {
     @EqualsAndHashCode.Include
     private Long id;
 
-    @NotBlank(message = "Nome é obrigatorio")
+    @NotBlank
     @Column(nullable = false)
     private String nome;
 
