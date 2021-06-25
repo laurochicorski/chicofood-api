f8077a80ab9382b49dd67c89d077d4c8bde14b96
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-06-24 21:21:38 -0300

    Criando e rodando um teste de integração com Spring Boot, JUnit e AssertJ

--- src/test/java/com/chicorski/chicofoodapi/CadastroCozinhaIntegrationTest.java
+++ src/test/java/com/chicorski/chicofoodapi/CadastroCozinhaIntegrationTest.java
@@ -1,19 +1,41 @@
 package com.chicorski.chicofoodapi;
 
+import com.chicorski.chicofoodapi.domain.model.Cozinha;
+import com.chicorski.chicofoodapi.domain.service.CadastroCozinhaService;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.junit.Assert.assertFalse;
+import javax.validation.ConstraintViolationException;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-class ChicofoodApiApplicationTests {
+public class CadastroCozinhaIntegrationTest {
+
+	@Autowired
+	private CadastroCozinhaService cadastroCozinha;
 
 	@Test
-	void contextLoads() {
-		assertFalse(true);
+	public void testarCadastroCozinhaComSucesso() {
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome("Chinesa");
+
+		novaCozinha = cadastroCozinha.salvar(novaCozinha);
+
+		assertThat(novaCozinha).isNotNull();
+		assertThat(novaCozinha.getId()).isNotNull();
+	}
+
+	@Test(expected = ConstraintViolationException.class)
+	public void testarCadastroCozinhaSemNome() {
+		Cozinha novaCozinha = new Cozinha();
+		novaCozinha.setNome(null);
+
+		novaCozinha = cadastroCozinha.salvar(novaCozinha);
 	}
 
 }
