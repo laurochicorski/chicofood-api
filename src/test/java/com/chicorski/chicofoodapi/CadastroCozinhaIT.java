09b58a0e613c56fa0bd8b811823782236350cfff
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-06-27 19:51:22 -0300

    Limpando e populando o banco de dados de teste

--- src/test/java/com/chicorski/chicofoodapi/CadastroCozinhaIT.java
+++ src/test/java/com/chicorski/chicofoodapi/CadastroCozinhaIT.java
@@ -1,10 +1,10 @@
 package com.chicorski.chicofoodapi;
 
+import com.chicorski.chicofoodapi.domain.model.Cozinha;
+import com.chicorski.chicofoodapi.domain.repository.CozinhaRepository;
+import com.chicorski.chicofoodapi.util.DatabaseCleaner;
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
-import org.flywaydb.core.Flyway;
-import org.hamcrest.Matcher;
-import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -28,7 +28,10 @@ public class CadastroCozinhaIT {
     private int port;
 
     @Autowired
-    private Flyway flyway;
+    private DatabaseCleaner databaseCleaner;
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
 
     @Before
     public void setUp() {
@@ -36,7 +39,8 @@ public class CadastroCozinhaIT {
         RestAssured.port = port;
         RestAssured.basePath = "/cozinhas";
 
-        flyway.migrate();
+        databaseCleaner.clearTables();
+        prepararDados();
     }
 
 
@@ -58,8 +62,7 @@ public class CadastroCozinhaIT {
         .when()
                 .get()
         .then()
-                .body("", hasSize(4))
-                .body("nome", hasItems("Indiana", "Tailandesa"));
+                .body("", hasSize(2));
     }
 
     @Test
@@ -72,6 +75,17 @@ public class CadastroCozinhaIT {
                 .post()
                 .then()
                 .statusCode(HttpStatus.CREATED.value());
+    }
+
+    private void prepararDados() {
+        Cozinha cozinha1 = new Cozinha();
+        cozinha1.setNome("Tailandesa");
+        cozinhaRepository.save(cozinha1);
+
+        Cozinha cozinha2 = new Cozinha();
+        cozinha2.setNome("Brasileira");
+        cozinhaRepository.save(cozinha2);
+
     }
 
 }
