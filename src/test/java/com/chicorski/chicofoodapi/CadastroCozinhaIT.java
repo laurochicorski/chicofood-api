12a689e5a9e1574ecea0d05677acb700c8e5e246
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-06-27 19:35:45 -0300

    Voltando o estado inicial do banco de dados para cada execução de teste com callback do Flyway

--- src/test/java/com/chicorski/chicofoodapi/CadastroCozinhaIT.java
+++ src/test/java/com/chicorski/chicofoodapi/CadastroCozinhaIT.java
@@ -2,11 +2,13 @@ package com.chicorski.chicofoodapi;
 
 import io.restassured.RestAssured;
 import io.restassured.http.ContentType;
+import org.flywaydb.core.Flyway;
 import org.hamcrest.Matcher;
 import org.hamcrest.Matchers;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.http.HttpStatus;
@@ -23,11 +25,16 @@ public class CadastroCozinhaIT {
     @LocalServerPort
     private int port;
 
+    @Autowired
+    private Flyway flyway;
+
     @Before
     public void setUp() {
         RestAssured.enableLoggingOfRequestAndResponseIfValidationFails();
         RestAssured.port = port;
         RestAssured.basePath = "/cozinhas";
+
+        flyway.migrate();
     }
 
 
