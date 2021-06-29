80a5ef00c97f10beb62f9bf99881e7dc134e05de
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-06-29 10:37:56 -0300

    Refinando o payload de cadastro com @JsonIgnoreProperties

--- src/main/java/com/chicorski/chicofoodapi/domain/model/Restaurante.java
+++ src/main/java/com/chicorski/chicofoodapi/domain/model/Restaurante.java
@@ -4,6 +4,7 @@ import com.chicorski.chicofoodapi.core.validation.Groups;
 import com.chicorski.chicofoodapi.core.validation.Multiplo;
 import com.chicorski.chicofoodapi.core.validation.ValorZeroIncluiDescricao;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import org.hibernate.annotations.CreationTimestamp;
@@ -39,6 +40,7 @@ public class Restaurante {
     @Column(name = "taxa_frete", nullable = false)
     private BigDecimal taxaFrete;
 
+    @JsonIgnoreProperties(value = {"nome"}, allowGetters = true)
     @Valid
     @ConvertGroup(from = Default.class, to = Groups.CozinhaId.class)
     @NotNull
