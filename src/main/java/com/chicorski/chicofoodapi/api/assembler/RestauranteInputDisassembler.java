f8ba354fe57979be8b777cc9258273a02a09a98d
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-06-30 10:39:34 -0300

    Adicionando e usando o ModelMapper

--- src/main/java/com/chicorski/chicofoodapi/api/assembler/RestauranteInputDisassembler.java
+++ src/main/java/com/chicorski/chicofoodapi/api/assembler/RestauranteInputDisassembler.java
@@ -3,20 +3,17 @@ package com.chicorski.chicofoodapi.api.assembler;
 import com.chicorski.chicofoodapi.api.model.input.RestauranteInput;
 import com.chicorski.chicofoodapi.domain.model.Cozinha;
 import com.chicorski.chicofoodapi.domain.model.Restaurante;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
 public class RestauranteInputDisassembler {
 
+    @Autowired
+    private ModelMapper modelMapper;
+
     public Restaurante toDomainObject(RestauranteInput restauranteInput) {
-        Cozinha cozinha = new Cozinha();
-        cozinha.setId(restauranteInput.getCozinha().getId());
-
-        Restaurante restaurante = new Restaurante();
-        restaurante.setNome(restauranteInput.getNome());
-        restaurante.setTaxaFrete(restauranteInput.getTaxaFrete());
-        restaurante.setCozinha(cozinha);
-
-        return restaurante;
+        return modelMapper.map(restauranteInput, Restaurante.class);
     }
 }
