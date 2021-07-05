7dca8342151aa38ec9d79a23d6d6c5a69fc8a32e
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-07-05 11:38:15 -0300

    Adicionando endereço no modelo da representação do recurso de restaurante

--- src/main/java/com/chicorski/chicofoodapi/core/modelmapper/ModelMapperConfig.java
+++ src/main/java/com/chicorski/chicofoodapi/core/modelmapper/ModelMapperConfig.java
@@ -1,6 +1,8 @@
 package com.chicorski.chicofoodapi.core.modelmapper;
 
+import com.chicorski.chicofoodapi.api.model.EnderecoModel;
 import com.chicorski.chicofoodapi.api.model.RestauranteModel;
+import com.chicorski.chicofoodapi.domain.model.Endereco;
 import com.chicorski.chicofoodapi.domain.model.Restaurante;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
@@ -15,6 +17,10 @@ public class ModelMapperConfig {
 
 //        modelMapper.createTypeMap(Restaurante.class, RestauranteModel.class)
 //                .addMapping(Restaurante::getTaxaFrete, RestauranteModel::setPrecoFrete);
+
+        var enderecoModelTypeMap = modelMapper.createTypeMap(Endereco.class, EnderecoModel.class);
+
+        enderecoModelTypeMap.<String>addMapping(src -> src.getCidade().getEstado().getNome(), (dest, value) -> dest.getCidade().setEstado(value));
 
         return modelMapper;
     }
