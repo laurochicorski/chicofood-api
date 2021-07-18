f28d735b3f984c513e5d532fb97c2cf43421518d
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-07-18 11:49:58 -0300

    13.2. Limitando os campos retornados pela API com @JsonFilter do Jackson

--- src/main/java/com/chicorski/chicofoodapi/api/controller/PedidoController.java
+++ src/main/java/com/chicorski/chicofoodapi/api/controller/PedidoController.java
@@ -12,8 +12,12 @@ import com.chicorski.chicofoodapi.domain.model.Pedido;
 import com.chicorski.chicofoodapi.domain.model.Usuario;
 import com.chicorski.chicofoodapi.domain.repository.PedidoRepository;
 import com.chicorski.chicofoodapi.domain.service.EmissaoPedidoService;
+import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
+import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -39,11 +43,30 @@ public class PedidoController {
     private PedidoInputDisassembler pedidoInputDisassembler;
 
     @GetMapping
-    public List<PedidoResumoModel> listar() {
+    public MappingJacksonValue listar(@RequestParam(required = false) String campos) {
         List<Pedido> todosPedidos = pedidoRepository.findAll();
+        List<PedidoResumoModel> pedidosModel = pedidoResumoModelAssembler.toCollectionModel(todosPedidos);
 
-        return pedidoResumoModelAssembler.toCollectionModel(todosPedidos);
+        MappingJacksonValue pedidosWrapper = new MappingJacksonValue(pedidosModel);
+
+        SimpleFilterProvider filterProvider = new SimpleFilterProvider();
+        filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.serializeAll());
+
+        if (StringUtils.isNotBlank(campos)) {
+            filterProvider.addFilter("pedidoFilter", SimpleBeanPropertyFilter.filterOutAllExcept(campos.split(",")));
+        }
+
+        pedidosWrapper.setFilters(filterProvider);
+
+        return pedidosWrapper;
     }
+
+//    @GetMapping
+//    public List<PedidoResumoModel> listar() {
+//        List<Pedido> todosPedidos = pedidoRepository.findAll();
+//
+//        return pedidoResumoModelAssembler.toCollectionModel(todosPedidos);
+//    }
 
     @GetMapping("/{codigo}")
     public PedidoModel buscar(@PathVariable String codigo) {
