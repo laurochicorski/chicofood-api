accda7e0dd42da6c19d56c71783fc8ff5fc43b8a
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-07-21 15:40:09 -0300

    13.20. Preenchendo um relatório JasperReports com JavaBeans e gerando bytes do PDF

--- src/main/java/com/chicorski/chicofoodapi/infrastructure/service/report/PdfVendaReportService.java
+++ src/main/java/com/chicorski/chicofoodapi/infrastructure/service/report/PdfVendaReportService.java
@@ -1,15 +1,43 @@
 package com.chicorski.chicofoodapi.infrastructure.service.report;
 
 import com.chicorski.chicofoodapi.domain.filter.VendaDiariaFilter;
+import com.chicorski.chicofoodapi.domain.service.VendaQueryService;
 import com.chicorski.chicofoodapi.domain.service.VendaReportService;
+import net.sf.jasperreports.engine.JRException;
+import net.sf.jasperreports.engine.JasperExportManager;
+import net.sf.jasperreports.engine.JasperFillManager;
+import net.sf.jasperreports.engine.JasperPrint;
+import net.sf.jasperreports.engine.data.JRBeanCollectionDataSource;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Locale;
 
 @Service
 public class PdfVendaReportService implements VendaReportService {
 
+    @Autowired
+    private VendaQueryService vendaQueryService;
 
     @Override
     public byte[] emitirVendasDiarias(VendaDiariaFilter filtro, String timeOffSet) {
-        return null;
+        try {
+            var inputStream = this.getClass().getResourceAsStream("/relatorios/vendas-diarias.jasper");
+
+            var parametros = new HashMap<String, Object>();
+            parametros.put("REPORT_LOCALE", new Locale("pt", "BR"));
+
+            var vendasDiarias = vendaQueryService.consultarVendasDiarias(filtro, timeOffSet);
+            var dataSource = new JRBeanCollectionDataSource(vendasDiarias);
+
+            JasperPrint jasperPrint = null;
+
+            jasperPrint = JasperFillManager.fillReport(inputStream, parametros, dataSource);
+
+            return JasperExportManager.exportReportToPdf(jasperPrint);
+        } catch (Exception e) {
+            throw new ReportException("Não foi possível emitir o relatório de vendas diárias.pdf", e.getCause());
+        }
     }
 }
