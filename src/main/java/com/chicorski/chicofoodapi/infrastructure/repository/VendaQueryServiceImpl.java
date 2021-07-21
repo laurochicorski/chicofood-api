01c761393bba797aebfd09b74df8ba3bf99a8ea4
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-07-21 11:47:59 -0300

    Desafio: adicionando os filtros na consulta de vendas diárias

--- src/main/java/com/chicorski/chicofoodapi/infrastructure/repository/VendaQueryServiceImpl.java
+++ src/main/java/com/chicorski/chicofoodapi/infrastructure/repository/VendaQueryServiceImpl.java
@@ -2,13 +2,16 @@ package com.chicorski.chicofoodapi.infrastructure.repository;
 
 import com.chicorski.chicofoodapi.domain.filter.VendaDiariaFilter;
 import com.chicorski.chicofoodapi.domain.model.Pedido;
+import com.chicorski.chicofoodapi.domain.model.StatusPedido;
 import com.chicorski.chicofoodapi.domain.model.dto.VendaDiaria;
 import com.chicorski.chicofoodapi.domain.service.VendaQueryService;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.criteria.Predicate;
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.List;
 
 @Repository
@@ -33,6 +36,27 @@ public class VendaQueryServiceImpl implements VendaQueryService {
 
         query.select(selection);
         query.groupBy(functionDateDataCriacao);
+
+        var predicates = new ArrayList<Predicate>();
+
+        if (filtro.getRestauranteId() != null) {
+            predicates.add(builder.equal(root.get("restaurante"), filtro.getRestauranteId()));
+        }
+
+        if (filtro.getDataCriacaoInicio() != null) {
+            predicates.add(builder.greaterThanOrEqualTo(root.get("dataCriacao"),
+                    filtro.getDataCriacaoInicio()));
+        }
+
+        if (filtro.getDataCriacaoFim() != null) {
+            predicates.add(builder.lessThanOrEqualTo(root.get("dataCriacao"),
+                    filtro.getDataCriacaoFim()));
+        }
+
+        predicates.add(root.get("status").in(
+                StatusPedido.CONFIRMADO, StatusPedido.ENTREGUE));
+
+        query.where(predicates.toArray(new Predicate[0]));
 
         return manager.createQuery(query).getResultList();
     }
