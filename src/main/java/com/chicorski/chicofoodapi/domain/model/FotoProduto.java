0f2741b7ec28adbea51e08a20dbc02451d0b8998
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-08-01 01:26:35 -0300

    14.5. Mapeando entidade FotoProduto e relacionamento um-para-um

---
+++ src/main/java/com/chicorski/chicofoodapi/domain/model/FotoProduto.java
@@ -0,0 +1,29 @@
+package com.chicorski.chicofoodapi.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class FotoProduto {
+
+    @EqualsAndHashCode.Include
+    @Id
+    @Column(name = "produto_id")
+    private Long id;
+
+    private String nomeArquivo;
+
+    private String descricao;
+
+    private String contentType;
+
+    private Long tamanho;
+
+    @OneToOne(fetch = FetchType.LAZY)
+    @MapsId
+    private Produto produto;
+}
