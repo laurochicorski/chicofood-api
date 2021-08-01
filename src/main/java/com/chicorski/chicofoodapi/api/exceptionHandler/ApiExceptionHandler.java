3d0d2434c832c850c4b8df7d1a93eda5f6429d9b
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-08-01 18:36:09 -0300

    14.16. Corrigindo exception handler de media type não aceita

--- src/main/java/com/chicorski/chicofoodapi/api/exceptionHandler/ApiExceptionHandler.java
+++ src/main/java/com/chicorski/chicofoodapi/api/exceptionHandler/ApiExceptionHandler.java
@@ -19,6 +19,7 @@ import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.validation.BindException;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.FieldError;
+import org.springframework.web.HttpMediaTypeNotAcceptableException;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -260,6 +261,11 @@ public class ApiExceptionHandler extends ResponseEntityExceptionHandler {
         }
 
         return super.handleExceptionInternal(ex, body, headers, status, request);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleHttpMediaTypeNotAcceptable(HttpMediaTypeNotAcceptableException ex, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        return ResponseEntity.status(status).headers(headers).build();
     }
 
     private Problem.ProblemBuilder createProblemBuilder(HttpStatus status, ProblemType type, String detail) {
