662dcfe10c469e7842f28b6889a9a75db1073046
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-08-01 16:48:49 -0300

    14.11. Desafio: Implementando recuperação de foto no serviço de armazenagem

--- src/main/java/com/chicorski/chicofoodapi/infrastructure/service/storage/LocalFotoStorageService.java
+++ src/main/java/com/chicorski/chicofoodapi/infrastructure/service/storage/LocalFotoStorageService.java
@@ -5,10 +5,9 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.util.FileCopyUtils;
 
-import java.io.IOException;
+import java.io.InputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
-import java.util.UUID;
 
 @Service
 public class LocalFotoStorageService implements FotoStorageService {
@@ -17,13 +16,24 @@ public class LocalFotoStorageService implements FotoStorageService {
     private Path diretorioFotos;
 
     @Override
+    public InputStream recuperar(String nomeArquivo) {
+        try {
+            Path arquivoPath = getArquivoPath(nomeArquivo);
+
+            return Files.newInputStream(arquivoPath);
+        } catch (Exception e) {
+            throw new StorageException("Não foi possível recuperar o arquivo.", e);
+        }
+    }
+
+    @Override
     public void armazenar(NovaFoto novaFoto) {
         try {
 
             Path arquivoPath = getArquivoPath(novaFoto.getNomeArquivo());
 
             FileCopyUtils.copy(novaFoto.getInputStream(), Files.newOutputStream(arquivoPath));
-        } catch (IOException e) {
+        } catch (Exception e) {
             throw new StorageException("Não foi possível armazenar arquivo.", e);
         }
     }
@@ -33,7 +43,7 @@ public class LocalFotoStorageService implements FotoStorageService {
         try {
             Path arquivoPath = getArquivoPath(nomeArquivo);
             Files.deleteIfExists(arquivoPath);
-        } catch (IOException e) {
+        } catch (Exception e) {
             throw new StorageException("Não foi possível excluir arquivo.", e);
         }
     }
