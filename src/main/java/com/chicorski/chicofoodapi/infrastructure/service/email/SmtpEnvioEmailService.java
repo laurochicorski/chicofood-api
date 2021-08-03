94aa4a7db57b5abe52daaaf19eb002b764fcc387
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-08-03 09:50:32 -0300

    15.5. Processando template de corpo de e-mails com Apache FreeMaker

--- src/main/java/com/chicorski/chicofoodapi/infrastructure/service/email/SmtpEnvioEmailService.java
+++ src/main/java/com/chicorski/chicofoodapi/infrastructure/service/email/SmtpEnvioEmailService.java
@@ -2,13 +2,17 @@ package com.chicorski.chicofoodapi.infrastructure.service.email;
 
 import com.chicorski.chicofoodapi.core.email.EmailProperties;
 import com.chicorski.chicofoodapi.domain.service.EnvioEmailService;
+import freemarker.template.Configuration;
+import freemarker.template.Template;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
+import org.springframework.ui.freemarker.FreeMarkerTemplateUtils;
 
 import javax.mail.MessagingException;
 import javax.mail.internet.MimeMessage;
+import java.io.IOException;
 
 @Service
 public class SmtpEnvioEmailService implements EnvioEmailService {
@@ -19,15 +23,20 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
     @Autowired
     private EmailProperties emailProperties;
 
+    @Autowired
+    private Configuration freemakerConfig;
+
     @Override
     public void enviar(Mensagem mensagem) {
         try {
+            String corpo = processarTemplate(mensagem);
+
             MimeMessage mimeMessage = mailSender.createMimeMessage();
 
             MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, "UTF-8");
             helper.setFrom(emailProperties.getRemetente());
             helper.setSubject(mensagem.getAssunto());
-            helper.setText(mensagem.getCorpo(), true);
+            helper.setText(corpo, true);
             helper.setTo(mensagem.getDestinatarios().toArray(new String[0]));
 
 
@@ -37,4 +46,14 @@ public class SmtpEnvioEmailService implements EnvioEmailService {
         }
     }
 
+    private String processarTemplate(Mensagem mensagem) {
+        try {
+            Template template = freemakerConfig.getTemplate(mensagem.getCorpo());
+
+            return FreeMarkerTemplateUtils.processTemplateIntoString(template,
+                    mensagem.getVariaveis());
+        } catch (Exception e) {
+            throw new EmailException("Não foi possível montar template do e-mail", e);
+        }
+    }
 }
