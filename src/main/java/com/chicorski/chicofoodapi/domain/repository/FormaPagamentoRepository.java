1edaeda436fbc4c627357afbba69a9eedc815d17
Lauro Chicorski <laurocoelhoc@gmail.com>
2021-08-16 20:11:54 -0300

    17.8. Entendendo e preparando a implementação de Deep ETags

--- src/main/java/com/chicorski/chicofoodapi/domain/repository/FormaPagamentoRepository.java
+++ src/main/java/com/chicorski/chicofoodapi/domain/repository/FormaPagamentoRepository.java
@@ -2,9 +2,13 @@ package com.chicorski.chicofoodapi.domain.repository;
 
 import com.chicorski.chicofoodapi.domain.model.FormaPagamento;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.OffsetDateTime;
 import java.util.List;
 
 public interface FormaPagamentoRepository extends JpaRepository<FormaPagamento, Long> {
 
+    @Query("select max(dataAtualizacao) from forma_pagamento")
+    OffsetDateTime getDataUtimaAtualizacao();
 }
