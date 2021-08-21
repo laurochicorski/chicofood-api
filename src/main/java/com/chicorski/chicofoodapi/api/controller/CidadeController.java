bbde62ad77225770bceb91bac642e7146110ce73
Lauro <laurocoelhoc@gmail.com>
2021-08-21 11:29:02 -0300

    18.17. Desacoplando anotações do Swagger dos controladores

--- src/main/java/com/chicorski/chicofoodapi/api/controller/CidadeController.java
+++ src/main/java/com/chicorski/chicofoodapi/api/controller/CidadeController.java
@@ -2,7 +2,7 @@ package com.chicorski.chicofoodapi.api.controller;
 
 import com.chicorski.chicofoodapi.api.assembler.CidadeInputDisassembler;
 import com.chicorski.chicofoodapi.api.assembler.CidadeModelAssembler;
-import com.chicorski.chicofoodapi.api.exceptionHandler.Problem;
+import com.chicorski.chicofoodapi.api.controller.openapi.CidadeControllerOpenApi;
 import com.chicorski.chicofoodapi.api.model.CidadeModel;
 import com.chicorski.chicofoodapi.api.model.input.CidadeInput;
 import com.chicorski.chicofoodapi.domain.exception.EstadoNaoEncontradaException;
@@ -10,10 +10,6 @@ import com.chicorski.chicofoodapi.domain.exception.NegocioException;
 import com.chicorski.chicofoodapi.domain.model.Cidade;
 import com.chicorski.chicofoodapi.domain.repository.CidadeRepository;
 import com.chicorski.chicofoodapi.domain.service.CadastroCidadeService;
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
@@ -21,10 +17,10 @@ import org.springframework.web.bind.annotation.*;
 import javax.validation.Valid;
 import java.util.List;
 
-@Api(tags = "Cidades")
+
 @RestController
 @RequestMapping("/cidades")
-public class CidadeController {
+public class CidadeController implements CidadeControllerOpenApi {
 
     @Autowired
     private CidadeModelAssembler cidadeModelAssembler;
@@ -38,32 +34,23 @@ public class CidadeController {
     @Autowired
     private CadastroCidadeService cadastroCidade;
 
-    @ApiOperation("Lista as cidades")
     @GetMapping
-    private List<CidadeModel> listar() {
+    public List<CidadeModel> listar() {
         List<Cidade> todasCidades = cidadeRepository.findAll();
 
         return cidadeModelAssembler.toCollectionModel(todasCidades);
     }
 
-    @ApiOperation("Busca cidade por id")
-    @ApiResponses({
-            @ApiResponse(code = 400, message = "ID da cidade inválido", response = Problem.class)
-    })
     @GetMapping("/{id}")
-    private CidadeModel buscar(@PathVariable Long id) {
+    public CidadeModel buscar(@PathVariable Long id) {
         Cidade cidade = cadastroCidade.buscarOuFalhar(id);
 
         return cidadeModelAssembler.toModel(cidade);
     }
 
-    @ApiOperation("Inclui uma cidade por id")
     @PostMapping
-    @ApiResponses({
-            @ApiResponse(code = 201, message = "Cidade cadastrada")
-    })
     @ResponseStatus(HttpStatus.CREATED)
-    private CidadeModel adicionar(@RequestBody @Valid CidadeInput cidadeInput) {
+    public CidadeModel adicionar(@RequestBody @Valid CidadeInput cidadeInput) {
         try {
             Cidade cidade = cidadeInputDisassembler.toDomainObject(cidadeInput);
 
@@ -76,13 +63,8 @@ public class CidadeController {
 
     }
 
-    @ApiOperation("Edita uma cidade por id")
-    @ApiResponses({
-            @ApiResponse(code = 200, message = "Cidade atualizada"),
-            @ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class)
-    })
     @PutMapping("/{id}")
-    private CidadeModel atualizar(@PathVariable Long id, @RequestBody @Valid CidadeInput cidadeInput) {
+    public CidadeModel atualizar(@PathVariable Long id, @RequestBody @Valid CidadeInput cidadeInput) {
         try {
             Cidade cidadeAtual = cadastroCidade.buscarOuFalhar(id);
 
@@ -96,14 +78,8 @@ public class CidadeController {
         }
     }
 
-    @ApiOperation("Exclui uma cidade por id")
-    @ApiResponses({
-            @ApiResponse(code = 204, message = "Cidade excluída"),
-            @ApiResponse(code = 404, message = "Cidade não encontrada", response = Problem.class)
-    })
     @DeleteMapping("/{id}")
-    @ResponseStatus(HttpStatus.NO_CONTENT)
-    private void remover(@PathVariable Long id) {
+    public void remover(@PathVariable Long id) {
         cadastroCidade.excluir(id);
     }
 }
