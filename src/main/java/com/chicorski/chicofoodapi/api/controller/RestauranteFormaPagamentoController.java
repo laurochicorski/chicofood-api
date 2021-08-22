1ba38e98e45d3f91f28369535881f1f289d05390
Lauro <laurocoelhoc@gmail.com>
2021-08-22 10:48:58 -0300

    18.32. Desafio: descrevendo documentação de endpoints de associação de restaurantes com formas de pagamento

--- src/main/java/com/chicorski/chicofoodapi/api/controller/RestauranteFormaPagamentoController.java
+++ src/main/java/com/chicorski/chicofoodapi/api/controller/RestauranteFormaPagamentoController.java
@@ -2,17 +2,19 @@ package com.chicorski.chicofoodapi.api.controller;
 
 import com.chicorski.chicofoodapi.api.assembler.FormaPagamentoModelAssembler;
 import com.chicorski.chicofoodapi.api.model.FormaPagamentoModel;
+import com.chicorski.chicofoodapi.api.openapi.controller.RestauranteFormaPagamentoControllerOpenApi;
 import com.chicorski.chicofoodapi.domain.model.Restaurante;
 import com.chicorski.chicofoodapi.domain.service.CadastroRestauranteService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("/restaurantes/{restauranteId}/formas-pagamento")
-public class RestauranteFormaPagamentoController {
+@RequestMapping(path = "/restaurantes/{restauranteId}/formas-pagamento", produces = MediaType.APPLICATION_JSON_VALUE)
+public class RestauranteFormaPagamentoController implements RestauranteFormaPagamentoControllerOpenApi {
 
     @Autowired
     private CadastroRestauranteService cadastroRestaurante;
