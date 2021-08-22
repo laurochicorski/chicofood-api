539afb32fe0dfe01888747dc1946ec1b792c3e1c
Lauro <laurocoelhoc@gmail.com>
2021-08-22 11:38:02 -0300

    18.39. Desafio: descrevendo documentação de endpoints de associação de grupos com usuários

--- src/main/java/com/chicorski/chicofoodapi/api/controller/UsuarioGrupoController.java
+++ src/main/java/com/chicorski/chicofoodapi/api/controller/UsuarioGrupoController.java
@@ -2,17 +2,19 @@ package com.chicorski.chicofoodapi.api.controller;
 
 import com.chicorski.chicofoodapi.api.assembler.GrupoModelAssembler;
 import com.chicorski.chicofoodapi.api.model.GrupoModel;
+import com.chicorski.chicofoodapi.api.openapi.controller.UsuarioGrupoControllerOpenApi;
 import com.chicorski.chicofoodapi.domain.model.Usuario;
 import com.chicorski.chicofoodapi.domain.service.CadastroUsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
-@RequestMapping(value = "/usuarios/{usuarioId}/grupos")
-public class UsuarioGrupoController {
+@RequestMapping(value = "/usuarios/{usuarioId}/grupos", produces = MediaType.APPLICATION_JSON_VALUE)
+public class UsuarioGrupoController implements UsuarioGrupoControllerOpenApi {
 
     @Autowired
     private CadastroUsuarioService cadastroUsuario;
