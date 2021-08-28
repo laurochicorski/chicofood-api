43d0558318c49c0bd2199a09cd1052b224634d29
Lauro <laurocoelhoc@gmail.com>
2021-08-28 20:51:05 -0300

    19.41. Corrigindo a paginação na documentação

--- src/main/java/com/chicorski/chicofoodapi/api/openapi/model/CozinhasModelOpenApi.java
+++ src/main/java/com/chicorski/chicofoodapi/api/openapi/model/CozinhasModelOpenApi.java
@@ -1,12 +1,28 @@
 package com.chicorski.chicofoodapi.api.openapi.model;
 
-import com.chicorski.chicofoodapi.api.model.CozinhaModel;
+import com.chicorski.chicofoodapi.api.model.CidadeModel;
+import com.chicorski.chicofoodapi.core.springfox.CidadesModelOpenApi;
 import io.swagger.annotations.ApiModel;
+import lombok.Data;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.hateoas.Link;
+
+import java.util.List;
 
 @ApiModel("CozinhasModel")
-public class CozinhasModelOpenApi extends PagedModelOpenApi<CozinhaModel> {
+@Setter
+@Getter
+public class CozinhasModelOpenApi  {
 
+    private CidadesModelOpenApi.CidadeEmbeddedModelOpenApi _embedded;
+    private Link _links;
+    private PageModelOpenApi page;
+
+    @ApiModel("CozinhasEmbeddedModel")
+    @Data
+    public class CozinhaEmbeddedModelOpenApi {
+        private List<CidadeModel> cozinhas;
+    }
 
 }
