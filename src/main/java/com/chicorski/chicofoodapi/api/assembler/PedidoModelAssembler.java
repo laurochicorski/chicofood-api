57008e0dff55213c1660d3ccea50654585b60a10
Lauro <laurocoelhoc@gmail.com>
2021-08-27 21:09:07 -0300

    19.23. Adicionando links condicionalmente

--- src/main/java/com/chicorski/chicofoodapi/api/assembler/PedidoModelAssembler.java
+++ src/main/java/com/chicorski/chicofoodapi/api/assembler/PedidoModelAssembler.java
@@ -31,9 +31,17 @@ public class PedidoModelAssembler extends RepresentationModelAssemblerSupport<Pe
 
         pedidoModel.add(chicoLinks.linkToPedidos());
 
-        pedidoModel.add(chicoLinks.linkToConfirmacaoPedido(pedido.getCodigo(), "confirmar"));
-        pedidoModel.add(chicoLinks.linkToEntregaPedido(pedido.getCodigo(), "entregar"));
-        pedidoModel.add(chicoLinks.linkToCancelamentoPedido(pedido.getCodigo(), "cancelar"));
+        if (pedido.podeSerConfirmado()) {
+            pedidoModel.add(chicoLinks.linkToConfirmacaoPedido(pedido.getCodigo(), "confirmar"));
+        }
+
+        if (pedido.podeSerEntregue()) {
+            pedidoModel.add(chicoLinks.linkToEntregaPedido(pedido.getCodigo(), "entregar"));
+        }
+
+        if (pedido.podeSerCancelado()) {
+            pedidoModel.add(chicoLinks.linkToCancelamentoPedido(pedido.getCodigo(), "cancelar"));
+        }
 
         pedidoModel.getRestaurante().add(
                 chicoLinks.linkToRestaurante(pedido.getRestaurante().getId()));
