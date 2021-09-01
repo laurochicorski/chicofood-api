cc11928f2ada01b808c595e0d067cae52779e34e
Lauro <laurocoelhoc@gmail.com>
2021-08-31 22:42:17 -0300

    20.19. Desligando uma versão da API

--- src/main/java/com/chicorski/chicofoodapi/core/web/ApiRetirementHandler.java
+++ src/main/java/com/chicorski/chicofoodapi/core/web/ApiRetirementHandler.java
@@ -1,5 +1,6 @@
 package com.chicorski.chicofoodapi.core.web;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
@@ -7,12 +8,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 @Component
-public class ApiDeprecationHandler extends HandlerInterceptorAdapter {
+public class ApiRetirementHandler extends HandlerInterceptorAdapter {
 
     @Override
     public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
         if (request.getRequestURI().startsWith("/v1")) {
-            response.addHeader("X-ChicoFood-Decrecated", "Versão depreciada. Use a versão mais recente");
+            response.setStatus(HttpStatus.GONE.value());
+            return false;
         }
 
         return true;
