791d07edfd9f4e14bead94d7d07d90259199d004
Lauro <laurocoelhoc@gmail.com>
2021-09-11 21:52:00 -0300

    23.30. Desafio: restringindo acessos ao endpoint de pesquisa de pedidos

--- src/main/java/com/chicorski/chicofoodapi/api/v1/controller/PedidoController.java
+++ src/main/java/com/chicorski/chicofoodapi/api/v1/controller/PedidoController.java
@@ -57,6 +57,7 @@ public class PedidoController implements PedidoControllerOpenApi {
     @Autowired
     private ChicoFoodSecurity chicoFoodSecurity;
 
+    @CheckSecurity.Pedidos.PodePesquisar
     @GetMapping
     public PagedModel<PedidoResumoModel> pesquisar(PedidoFilter filtro,
                                                    @PageableDefault(size=10) Pageable pageable) {
